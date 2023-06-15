9dff85719e993c8eeffca63ee66c5a8b95a80c4c
AhmirPostell <124746123+AhmirPostell@users.noreply.github.com>
2023-06-15 19:53:18 -0400

    finished ui of weather screen

---
+++ WeatherDemo/WeatherDemo/Components/WeatherRow.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherRow.swift
+//  WeatherDemo
+//
+//  Created by Ahmir Postell on 6/15/23.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherRow_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherRow()
+    }
+}
