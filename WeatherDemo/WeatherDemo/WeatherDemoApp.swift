c33cbbbaf09ed018e864b89e9550f85832d318b1
AhmirPostell <124746123+AhmirPostell@users.noreply.github.com>
2023-06-15 17:33:33 -0400

    setup swift project and location manager

---
+++ WeatherDemo/WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Ahmir Postell on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
